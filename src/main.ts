d3e189e2134c1ca5950e4e7af695cc9b3d379cfb
sergey.puzanov <sergey.puzanov@red-soft.ru>
2019-02-07 15:31:51 +0300

    create project

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueVue, { test } from 'vue-mapp-webpack';
+// const Test = require('vue-mapp-webpack');
+
+console.log(VueVue, test);
 
 Vue.config.productionTip = false;
+// Vue.use(VueMapp);
+
+// Vue.component('vm-button', VueMappButton);
+
 
 new Vue({
   router,
