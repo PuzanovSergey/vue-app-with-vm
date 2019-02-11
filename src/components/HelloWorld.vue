8049f7ad6474e0e25ed6aebc7c57e5c0b62ac9f7
sergey.puzanov <sergey.puzanov@red-soft.ru>
2019-02-11 15:23:21 +0300

    vue.config.js symlinks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <!-- <vm-button>sadfads</vm-button> -->
+    <vm-button>sadfads</vm-button>
+    <vm-checkbox>sdfa</vm-checkbox>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import VueVue, { test } from 'vue-mapp-webpack';
-// const Test = require('vue-mapp-webpack');
-
-console.log(VueVue, test);
+import VueMapp from 'vue-mapp-webpack';
+import 'vue-mapp-webpack/dist/full.css';
 
 Vue.config.productionTip = false;
-// Vue.use(VueMapp);
 
-// Vue.component('vm-button', VueMappButton);
-
+Vue.use(VueMapp);
+// Vue.component('vm-button-test', VueMappButton);
 
 new Vue({
   router,
